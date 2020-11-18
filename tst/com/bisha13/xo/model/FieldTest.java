1251a5fe134bd0922ce3afc13de876fe97dda001
Bisha13 <ershoww@gmail.com>
2020-11-18 22:54:07 +0300

    FieldTest test for exceptions have been added

--- tst/com/bisha13/xo/model/FieldTest.java
+++ tst/com/bisha13/xo/model/FieldTest.java
@@ -25,7 +25,23 @@ public class FieldTest {
 
         assertEquals(expectedFigure, actualFigure);
 
+    }
 
+    @Test
+    public void testSetFigureWhenAlreadyOccupied() throws Exception {
+
+        final Figure inputFigure = Figure.O;
+        final int fieldSize = 3;
+
+        final Field testField = new Field(fieldSize);
+        final Point point = new Point(0, 0);
+
+        testField.setFigure(point, inputFigure);
+
+        try {
+            testField.setFigure(point, inputFigure);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
     }
 
     @Test
@@ -41,4 +57,115 @@ public class FieldTest {
 
         assertEquals(expectedSize, actualSize);
     }
+
+    @Test
+    public void testGetFigureWhenFigureNotSet() throws Exception {
+
+        final int fieldSize = 3;
+
+        final Field testField = new Field(fieldSize);
+        final Point point = new Point(0, 0);
+
+        Figure actualFigure = testField.getFigure(point);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    public void testGetFigureWhenXLessThanZero() {
+
+        final int fieldSize = 3;
+
+        final Field testField = new Field(fieldSize);
+        final Point point = new Point(-1, 0);
+
+        try {
+            Figure actualFigure = testField.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {
+        }
+
+    }
+
+    @Test
+    public void testGetFigureWhenYLessThanZero() {
+
+        final int fieldSize = 3;
+
+        final Field testField = new Field(fieldSize);
+        final Point point = new Point(0, -1);
+
+        try {
+            Figure actualFigure = testField.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {
+        }
+
+    }
+
+    @Test
+    public void testGetFigureWhenBothLessThanZero() {
+
+        final int fieldSize = 3;
+
+        final Field testField = new Field(fieldSize);
+        final Point point = new Point(-1, -1);
+
+        try {
+            Figure actualFigure = testField.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {
+        }
+
+    }
+
+    @Test
+    public void testGetFigureWhenXMoreThanZero() {
+
+        final int fieldSize = 3;
+
+        final Field testField = new Field(fieldSize);
+        final Point point = new Point(testField.getSize() + 1, 0);
+
+        try {
+            Figure actualFigure = testField.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {
+        }
+
+    }
+
+    @Test
+    public void testGetFigureWhenYMoreThanZero() {
+
+        final int fieldSize = 3;
+
+        final Field testField = new Field(fieldSize);
+        final Point point = new Point(0, testField.getSize() + 1);
+
+        try {
+            Figure actualFigure = testField.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {
+        }
+
+    }
+
+    @Test
+    public void testGetFigureWhenBothMoreThanZero() {
+
+        final int fieldSize = 3;
+
+        final Field testField = new Field(fieldSize);
+        final Point point = new Point(testField.getSize() + 1, testField.getSize() + 1);
+
+        try {
+            Figure actualFigure = testField.getFigure(point);
+            fail();
+        } catch (final InvalidPointException e) {
+        }
+
+    }
+
+
 }
