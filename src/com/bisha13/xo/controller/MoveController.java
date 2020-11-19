0e250e0217fbc009a9a196f736db69dcd7b5186c
Bisha <ershoww@gmail.com>
2020-11-19 16:34:49 +0300

    MoveController created

---
+++ src/com/bisha13/xo/controller/MoveController.java
@@ -0,0 +1,21 @@
+package com.bisha13.xo.controller;
+
+import com.bisha13.xo.model.Field;
+import com.bisha13.xo.model.Figure;
+import com.bisha13.xo.model.Point;
+import com.bisha13.xo.model.exceptions.AlreadyOccupiedException;
+import com.bisha13.xo.model.exceptions.InvalidPointException;
+
+
+public class MoveController {
+
+    public void applyFigure(final Field field,
+                            final Point point,
+                            final Figure figure) throws InvalidPointException,
+                                                      AlreadyOccupiedException {
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
+    }
+}
