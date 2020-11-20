655f0e2f9e9ae7d773aa8cd5ea9436af36a9cacd
Bisha <ershoww@gmail.com>
2020-11-20 17:02:35 +0300

    CurrentMoveControllerTest created

---
+++ tst/com/bisha13/xo/controller/CurrentMoveControllerTest.java
@@ -0,0 +1,56 @@
+package com.bisha13.xo.controller;
+
+import com.bisha13.xo.model.Field;
+import com.bisha13.xo.model.Figure;
+import com.bisha13.xo.model.Point;
+import com.bisha13.xo.model.exceptions.InvalidPointException;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CurrentMoveControllerTest {
+
+    @Test
+    public void testZeroFigures() throws InvalidPointException {
+        int fieldSize = 3;
+        Field field = new Field(fieldSize);
+        CurrentMoveController currentMoveController = new CurrentMoveController();
+        Figure expectedFigure = Figure.X;
+        Figure actualFigure = currentMoveController.currentMove(field);
+
+        assertEquals(expectedFigure, actualFigure);
+
+    }
+
+    @Test
+    public void testOddFigures() throws InvalidPointException {
+        int fieldSize = 3;
+        Field field = new Field(fieldSize);
+        field.setFigure(new Point(0, 0), Figure.X);
+
+        CurrentMoveController currentMoveController = new CurrentMoveController();
+
+        Figure expectedFigure = Figure.O;
+        Figure actualFigure = currentMoveController.currentMove(field);
+
+        assertEquals(expectedFigure, actualFigure);
+
+    }
+
+    @Test
+    public void testEvenFigures() throws InvalidPointException {
+        int fieldSize = 3;
+        Field field = new Field(fieldSize);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(0, 1), Figure.O);
+
+        CurrentMoveController currentMoveController = new CurrentMoveController();
+
+        Figure expectedFigure = Figure.X;
+        Figure actualFigure = currentMoveController.currentMove(field);
+
+        assertEquals(expectedFigure, actualFigure);
+
+    }
+
+}
