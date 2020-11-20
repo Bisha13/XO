0bf0dbc4e976fed7e1d14086764e743fd1d4d23b
Bisha13 <ershoww@gmail.com>
2020-11-20 23:01:36 +0300

    WinnerControllerTest checkWinnerWhenNoWinnerAndFieldFull added

--- tst/com/bisha13/xo/controller/WinnerControllerTest.java
+++ tst/com/bisha13/xo/controller/WinnerControllerTest.java
@@ -143,4 +143,27 @@ public class WinnerControllerTest {
         assertEquals(expectedFigure, actualFigure);
     }
 
+    @Test
+    public void checkWinnerWhenNoWinnerAndFieldFull() throws InvalidPointException {
+        final int fieldSize = 3;
+        final Field field = new Field(fieldSize);
+        field.setFigure(new Point(0, 0), Figure.X);
+        field.setFigure(new Point(1, 0), Figure.O);
+        field.setFigure(new Point(2, 0), Figure.O);
+
+        field.setFigure(new Point(0, 1), Figure.O);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 1), Figure.X);
+
+        field.setFigure(new Point(0, 2), Figure.O);
+        field.setFigure(new Point(1, 2), Figure.X);
+        field.setFigure(new Point(2, 2), Figure.O);
+
+        WinnerController winnerController = new WinnerController();
+        Figure expectedFigure = null;
+        Figure actualFigure = winnerController.getWinner(field);
+
+        assertEquals(expectedFigure, actualFigure);
+    }
+
 }
