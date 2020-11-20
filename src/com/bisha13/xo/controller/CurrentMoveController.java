a9a19c6c90869cf70f4180fd384d40bbc0a98c86
Bisha13 <ershoww@gmail.com>
2020-11-20 23:09:02 +0300

    CurrentMoveController refactored (inner cycle moved in private method)

--- src/com/bisha13/xo/controller/CurrentMoveController.java
+++ src/com/bisha13/xo/controller/CurrentMoveController.java
@@ -7,14 +7,24 @@ import com.bisha13.xo.model.exceptions.InvalidPointException;
 
 public class CurrentMoveController {
 
-    public Figure currentMove(Field field) throws InvalidPointException {
+    public Figure currentMove(final Field field) {
         int count = 0;
         for (int x = 0; x < field.getSize(); x++) {
-            for (int y = 0; y < field.getSize(); y++) {
-                if (field.getFigure(new Point(x, y)) != null)
-                    count++;
-            }
+            count += countFigureInRow(field, x);
         }
         return count % 2 == 0 ? Figure.X : Figure.O;
+    }
+
+    private int countFigureInRow(final Field field, final int row) {
+        int count = 0;
+        for (int y = 0; y < field.getSize(); y++) {
+            try {
+                if (field.getFigure(new Point(row, y)) != null)
+                    count++;
+            } catch (InvalidPointException e) {
+                e.printStackTrace();
+            }
+        }
+        return count;
     }
 }
